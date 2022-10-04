df5b54f95a207fde697645d43f5282d7204cccce
xLanStar <danny95624268@gmail.com>
2022-10-04 15:56:39 +0800

    Update dotenv

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from bot.instance import bot
+from dotenv import dotenv_values
 
-bot.run('MTAyNjc1NDM2NTU1NTQ3NDUwMw.GKpHMH.JinFbwaVExUEGwrDNQQ8Oc69XXrtun_4cdGTXs') #TOKEN在剛剛Discord Developer那邊「BOT」頁面裡面
+env = dotenv_values('.env')
+bot.run(env.get('token'))
